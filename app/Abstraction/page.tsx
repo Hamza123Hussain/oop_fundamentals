e8aac1d4eefc41fd3ff97252b439cbce0688c79e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-06 21:28:17 +0500

    notes added

--- app/Abstraction/page.tsx
+++ app/Abstraction/page.tsx
@@ -31,3 +31,14 @@ const Abstraction = () => {
 }
 
 export default Abstraction
+/**Abstract Methods Implementation:
+
+Renamed the abstract method gettypea to getType to match the method name in Car.
+The method getType in Car now returns the cartype value.
+Attribute Access:
+
+Changed cartype to protected so that it can be accessed in the Car subclass.
+Instance and Rendering:
+
+Created an instance of Car, used setType to modify the cartype, and displayed the result using getType in the React component.
+This code demonstrates abstraction by defining an abstract Vehicle class with essential methods and creating a concrete Car class that provides specific implementations. */
